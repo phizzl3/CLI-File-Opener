da98ea2870fe82115b653394c7112b10d6a30e76
Brandon Phillips <phizzl3@gmail.com>
2024-03-02 22:30:23 -0600

    Remove subprocess - only uses os and platform

--- openfile.py
+++ openfile.py
@@ -1,36 +1,11 @@
-"""
-
-Open a specified (passed) file path in it's default application
-based on detected Operating System.
-Supports Windows, Mac, & Linux.
-
-"""
-
-__version__ = "1.0.0"
-
+__version__ = "1.1.0"
 
 import os
 import platform
-import subprocess
 
 
 def open_file(file_path: str) -> None:
-    """Open specified file based on detected Operating System.
-    Supports Windows, Mac, & Linux.
-
-    Args:
-        file_path (str): File path for the file to be opened.
-    """
-    # Get Operating System
-    operating_system = platform.system()
-    # Subprocess Operating Systems and their associated commands.
-    subprocess_commands = {
-        "Darwin": f"open \"{file_path}\"",
-        "Linux": f"xdg-open \"{file_path}\"",
-    }
-    # Open based on detected operating system.
-    if operating_system == "Windows":
+    if platform.system() == "Windows":
         os.startfile(file_path)
-    if operating_system in subprocess_commands:
-        subprocess.run(subprocess_commands[operating_system],
-                       check=False, shell=True)
+    else:
+        os.system(f'open "{file_path}"')
